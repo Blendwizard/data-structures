fa5b05fe01b088c8968b6cdbd62a16ca3844af02
JoeVanCamp <vancampjoe@gmail.com>
2022-02-24 16:42:01 -0800

    Complete Queue and Stack for Functional Instantiation

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -4,16 +4,40 @@ var Queue = function() {
   // Use an object with numeric keys to store values
   var storage = {};
 
+  // Keep track of your numeric key
+  var key = 0;
+
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    // I: value
+    // O: none
+    // C: no arrays
+    // E: none
+    // Example: {1 : firstVal, 2 : secondVal, 3 : thirdVal}
+    storage[key] = value;
+    key++;
+
   };
 
   someInstance.dequeue = function() {
+    // We want to remove the first item we added
+    var keyList = Object.keys(storage);
+    var firstInKey = keyList[0];
+    var firstInItem = storage[firstInKey];
+
+    delete storage[firstInKey];
+    return firstInItem;
   };
 
   someInstance.size = function() {
+    var keyList = Object.keys(storage);
+    return keyList.length;
   };
 
   return someInstance;
 };
+
+// FIFO
+// Stack = 1 > 2 > 3 ? pop 1 > 2 > 1
+// Queue = 1 > 2 > 3 ? pop 1 > 2 > 3
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -4,15 +4,40 @@ var Stack = function() {
   // Use an object with numeric keys to store values
   var storage = {};
 
+  var key = 0;
+
   // Implement the methods below
   someInstance.push = function(value) {
+    // example: {1 : someValue, 2 : anotherValue, 3 : thirdValue}
+    storage[key] = value;
+    key++;
   };
 
   someInstance.pop = function() {
+    var keyList = Object.keys(storage);
+    var keyToPop = keyList[keyList.length - 1];
+    var poppedItem = storage[keyToPop];
+
+    key--;
+    delete storage[keyToPop];
+    return poppedItem;
   };
 
   someInstance.size = function() {
+    var keyList = Object.keys(storage);
+    return keyList.length;
+
   };
 
   return someInstance;
 };
+
+// Functional instantiation: a simple "maker" pattern
+
+// Do:
+// Work within the src/functional/ folder
+// Define all functions and properties within the maker function
+// Capitalize the maker function name
+// Don't:
+// Use the keyword new, the keyword this, or any prototype chains
+// Example: The provided classes Stack and Queue already follow this pattern
